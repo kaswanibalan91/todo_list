f93eaff020f7eea4fa4abef0f012c27b009aa2fc
Owner <Owner@ASWANI-CHRISTO>
2023-07-05 20:16:23 -0400

    Add project files.

---
+++ base/models.py
@@ -0,0 +1,22 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    create = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
